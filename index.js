6d663d054974d4f7a422277fa192ad232edaa2d9
Chris <christophe.crognier@gmail.com>
2025-09-19 09:31:33 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Connect to MongoDB
+// Si vous n'avez pas le console.log, vérifiez d'installer MondoDB Community Server et de le lancer en local !
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log('Connected to MongoDB');})
+.catch(err => {
+  console.error('Error connecting to MongoDB', err);});
+
+app.listen(port, () => {
+  console.log(`Server online at http://localhost:${port}`);
+});

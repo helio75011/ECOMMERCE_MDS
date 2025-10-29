a7fabee7bed36a4becd3535786622c34397c461b
de Breyne <111784301+helio75011@users.noreply.github.com>
2025-10-29 15:43:14 +0100

    add requireRole middleware

--- middlewares/requireRole.js
+++ middlewares/requireRole.js
@@ -1,7 +1,8 @@
-module.exports = (...roles) => (req, res, next) => {
-    if (!req.user) return res.status(401).json({ error: 'Unauthorized' });
-    if (!roles.includes(req.user.role)) {
-        return res.status(403).json({ error: 'Forbidden insufficient role' });
-    }
-    next();
+module.exports = function(requiredRole) {
+    return (req, res, next) => {
+        if (req.user.role !== requiredRole) {
+            return res.status(403).json({ error: "Accès interdit. Réservé à l'admin." });
+        }
+        next();
+    };
 };

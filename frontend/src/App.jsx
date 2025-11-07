269acc3e613e8589dcf7ecba5da8674938fc7f58
de Breyne <111784301+helio75011@users.noreply.github.com>
2025-11-07 14:48:19 +0100

    add tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+      <div className="">
+        <header className="">
+          <h1 class="text-3xl font-bold">Welcome to My React App</h1>
+          <p>This is a simple React application.</p>
+          <p>coucou</p>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
